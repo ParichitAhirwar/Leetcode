dc874a7387bf9932cc8b086cc90e73954fde6893
Parichit Ahirwar <parichitahirwar@gmail.com>
2026-01-27 23:17:19 +0530

    Time: 27 ms (50.76%) | Memory: 20.5 MB (25.23%) - LeetSync

---
+++ 55-jump-game/jump-game.py
@@ -0,0 +1,8 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        mr=0
+        for i in range(len(nums)):
+            if i>mr:
+                return False
+            mr=max(mr,i+nums[i])
+        return True
