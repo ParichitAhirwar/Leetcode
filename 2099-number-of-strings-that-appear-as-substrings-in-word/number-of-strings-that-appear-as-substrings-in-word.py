d79d4c9f57d055793bed38446cd865a97f9c225a
Parichit Ahirwar <parichitahirwar@gmail.com>
2026-06-29 14:44:32 +0530

    Time: 3 ms (13.33%) | Memory: 19.3 MB (19.78%) - LeetSync

--- 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.py
+++ 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.py
@@ -1,7 +1,3 @@
 class Solution:
     def numOfStrings(self, patterns: List[str], word: str) -> int:
-        c=0
-        for p in patterns:
-            if p in word:
-                c+=1
-        return c
+        return sum(p in word for p in patterns)
